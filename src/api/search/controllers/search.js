aeb09328e432081bdbc73f33d8b7e580dc909567
Paweł Niedźwiecki <hello@uxu.pl>
2022-02-02 15:54:39 +0100

    i midifed api search

--- src/api/search/controllers/search.js
+++ src/api/search/controllers/search.js
@@ -7,23 +7,47 @@
 module.exports = {
   async find(ctx, next) {
     const article = await strapi.db.query("api::articles.articles").findMany({
-      select: ["title"],
       where: {
         title: {
           $containsi: ctx.params.id,
         },
       },
+      orderBy: {
+        views: "desc",
+      },
+      limit: 15,
+      populate: ["cover", "tags"],
+      select: ["title", "views"],
+    });
+
+    const video = await strapi.db.query("api::video.video").findMany({
+      where: {
+        title: {
+          $containsi: ctx.params.id,
+        },
+      },
+      orderBy: {
+        views: "desc",
+      },
+      limit: 15,
+      populate: ["cover", "tags"],
+      select: ["title", "views"],
     });
 
     const tag = await strapi.db.query("api::tags.tags").findMany({
-      select: ["title"],
       where: {
         title: {
           $containsi: ctx.params.id,
         },
       },
+      orderBy: {
+        views: "desc",
+      },
+      limit: 15,
+      populate: ["cover"],
+      select: ["title", "views"],
     });
 
-    ctx.body = { article, tag };
+    ctx.body = { article, video, tag };
   },
 };

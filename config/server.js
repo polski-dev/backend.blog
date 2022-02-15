3db91566edcd32a273b8f8419b17b10369318167
Paweł Niedźwiecki <hello@uxu.pl>
2022-02-15 23:14:35 +0100

    i update server

--- config/server.js
+++ config/server.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   url: env("MY_HEROKU_URL"),
   host: env("HOST"),
-  port: env.int("PORT"),
+  port: env.int("PORT" || "1337"),
   app: {
-    keys: env.array("APP_KEYS"),
+    keys: env.array("APP_KEYS" || "http://localhost:1337"),
   },
 });

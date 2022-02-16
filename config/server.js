9534d919798d9dcc080c6d1de4f3be581590ccf9
Paweł Niedźwiecki <hello@uxu.pl>
2022-02-16 18:06:21 +0100

    update strapi

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  url: env("MY_HEROKU_URL" || "http://localhost:1337"),
+  url: env("MY_HEROKU_URL" || "http://localhost:1337/"),
   host: env("HOST"),
   port: env.int("PORT" || "1337"),
   app: {

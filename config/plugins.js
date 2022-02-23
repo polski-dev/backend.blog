51cda121f8da45fcc034299a5eca86b2fa3c3930
Paweł Niedźwiecki <hello@uxu.pl>
2022-02-23 23:40:11 +0100

    i update deep graphql

--- config/plugins.js
+++ config/plugins.js
@@ -33,7 +33,7 @@ module.exports = ({ env }) => ({
       endpoint: "/graphql",
       shadowCRUD: true,
       playgroundAlways: false,
-      depthLimit: 10,
+      depthLimit: 20,
       amountLimit: 100,
       apolloServer: {
         tracing: false,

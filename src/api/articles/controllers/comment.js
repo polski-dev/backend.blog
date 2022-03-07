f3eeee31ba8f4a9a7699d0e551cf7dbcd517e238
Paweł Niedźwiecki <hello@uxu.pl>
2022-03-07 23:59:20 +0100

    i added controller to add coments in article

--- src/api/articles/controllers/comment.js
+++ src/api/articles/controllers/comment.js
@@ -17,12 +17,11 @@ module.exports = {
   },
 
   async add(ctx, next) {
-    ctx.body = await strapi.db.query("api::comments.comments").findMany({
-      where: {
-        article: { id: ctx.params.id },
-      },
-      populate: {
-        grade: true,
+    ctx.body = await strapi.db.query("api::comments.comments").create({
+      data: {
+        description: ctx.request.body.description,
+        article: { id: ctx.request.params.id },
+        author: { id: ctx.state.user.id },
       },
     });
   },

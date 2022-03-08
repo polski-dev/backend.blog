c19f219302490f307a0e1b8fb2d290d68990b179
Paweł Niedźwiecki <hello@uxu.pl>
2022-03-08 20:59:20 +0100

    i update added comments in article

--- src/api/articles/controllers/comment.js
+++ src/api/articles/controllers/comment.js
@@ -17,12 +17,45 @@ module.exports = {
   },
 
   async add(ctx, next) {
-    ctx.body = await strapi.db.query("api::comments.comments").create({
-      data: {
-        description: ctx.request.body.description,
-        article: { id: ctx.request.params.id },
-        author: { id: ctx.state.user.id },
-      },
-    });
+    const description = ctx.request.body.description;
+    const articleId = ctx.request.params.id;
+    const userId = ctx.state.user.id;
+
+    if (!!description && !!articleId && !!userId) {
+      const addComment = await strapi.db.query("api::comments.comments").create({
+        data: {
+          description: ctx.request.body.description,
+          article: { id: ctx.request.params.id },
+          author: { id: ctx.state.user.id },
+        },
+      });
+
+      ctx.body = {
+        data: {
+          add: true,
+          id: addComment.id,
+          createdAt: addComment.createdAt,
+          description: addComment.description,
+        },
+      };
+    } else {
+      ctx.body = {
+        data: null,
+        error: {
+          status: 400,
+          name: "ValidationError",
+          message: `Field is a required field`,
+          details: {
+            errors: [
+              {
+                path: [`description: ${description}`, `articleId: ${articleId}`, `userId: ${userId}`],
+                message: `Field is a required field`,
+                name: "ValidationError",
+              },
+            ],
+          },
+        },
+      };
+    }
   },
 };

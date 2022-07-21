f321e65302a722eced1b9ad2f2e0647ca5911b12
Paweł Niedźwiecki <hello@uxu.pl>
2022-07-21 22:00:24 +0200

    i update script for adsence

--- src/api/users/services/dataget.js
+++ src/api/users/services/dataget.js
@@ -8,15 +8,11 @@ module.exports = {
   async dataPublicRead(ctx) {
     const data = await strapi.db.query("plugin::users-permissions.user").findOne({
       where: { id: ctx.state.user.id },
-      populate: {
-        avatar: {
-          select: ["url"],
-        },
-      },
+      populate: ["avatar"],
       select: ["id", "username", "blocked", "views", "createdAt", "updatedAt", "about", "website", "youtube", "instagram", "tiktok", "github", "city", "country"],
     });
 
-    return { data };
+    return { data: { ...data, avatar: { id: data?.avatar?.id, attributes: { ...data?.avatar } } } };
   },
 
   async dataEmailRead(ctx) {
